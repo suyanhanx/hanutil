7b9b82c12ec565ed7ccba85f17ea7e4aa811f732
han <suyanhanx@qq.com>
2019-03-28 18:40:09 +0800

    add single file build & compress

--- script/webpack.config.js
+++ script/webpack.config.js
@@ -1,26 +1,42 @@
-var pkg = require('../package.json')
+var pkg = require("../package.json");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
     filename: `${pkg.name}.min.js`,
     library: `${pkg.name}`,
-    libraryTarget: 'umd'
+    libraryTarget: "umd"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ["*", ".js"]
   },
   performance: {
     hints: false
-  }
-}
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        warning: "verbose",
+        ecma: 5,
+        output: {
+          comments: false,
+          beautify: false
+        },
+        compress: {
+          warnings: false
+        }
+      }
+    })
+  ]
+};
